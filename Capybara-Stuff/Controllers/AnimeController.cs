c4446ed55a3212315583c86f51d2ea59356025d3
Gleb <gleb.yashin@gmail.com>
2016-11-13 19:35:27 +0200

    Data Model generation

---
+++ Capybara-Stuff/Controllers/AnimeController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Capybara_Stuff.Models;
+
+namespace Capybara_Stuff.Controllers
+{
+    public class AnimeController : ApiController
+    {
+        private CapyStuffEntities db = new CapyStuffEntities();
+
+        // GET: api/Anime
+        public IQueryable<Anime> GetAnimes()
+        {
+            return db.Animes;
+        }
+
+        // GET: api/Anime/5
+        [ResponseType(typeof(Anime))]
+        public IHttpActionResult GetAnime(int id)
+        {
+            Anime anime = db.Animes.Find(id);
+            if (anime == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(anime);
+        }
+
+        // PUT: api/Anime/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutAnime(int id, Anime anime)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != anime.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(anime).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AnimeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Anime
+        [ResponseType(typeof(Anime))]
+        public IHttpActionResult PostAnime(Anime anime)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Animes.Add(anime);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = anime.Id }, anime);
+        }
+
+        // DELETE: api/Anime/5
+        [ResponseType(typeof(Anime))]
+        public IHttpActionResult DeleteAnime(int id)
+        {
+            Anime anime = db.Animes.Find(id);
+            if (anime == null)
+            {
+                return NotFound();
+            }
+
+            db.Animes.Remove(anime);
+            db.SaveChanges();
+
+            return Ok(anime);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AnimeExists(int id)
+        {
+            return db.Animes.Count(e => e.Id == id) > 0;
+        }
+    }
+}
